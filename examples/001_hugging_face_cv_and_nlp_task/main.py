b40b47bb66185f88e56ebf9bf0ea8b7f6a69187f
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2024-07-01 14:33:29 +0200

    Add libs and code to get and repr image in IDE

--- examples/001_hugging_face_cv_and_nlp_task/main.py
+++ examples/001_hugging_face_cv_and_nlp_task/main.py
@@ -5,7 +5,14 @@ import cv2
 # from google.colab.patches import cv2_imshow
 import os
 from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type
+import matplotlib.pyplot as plt
 
+
+def _is_interactive():
+    import __main__ as main
+    return not hasattr(main, '__file__')
+
+is_interactive = _is_interactive()
 
 token_access = os.environ.get("API_KEY_HUGGING_FACE")
 HEADERS = {
@@ -24,7 +31,7 @@ API_URL = "https://api-inference.huggingface.co/models/facebook/detr-resnet-50"
     stop=stop_after_attempt(5),
     retry=retry_if_exception_type(requests.exceptions.RequestException)
 )
-def query(filename):
+def get_cv_data(filename: str):
     with open(filename, "rb") as f:
         data = f.read()
     response = requests.request("POST", API_URL, headers=HEADERS, data=data)
@@ -37,7 +44,28 @@ def query(filename):
 
     return data
 
+def get_image_data(filename: str):
+    image = cv2.imread(filename)
+
+    #Show image
+    if is_interactive:
+        pass
+        # from google.colab.patches import cv2_imshow
+        # cv2_imshow(image)
+    else:
+        # cv2.imshow('Savanna Image', image)
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)) # Convert the image from BGR to RGB color space
+        plt.title("Savanna Image")
+        plt.show()
+
+        # Wait for a key press and close the window
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
 
 if __name__ == "__main__":
-    data = query("savana.jpg")
-    print(data)
+    image = "savanna.jpg"
+    image_data = get_image_data(image)
+    cv_data = get_cv_data(image)
+    print(image_data)
