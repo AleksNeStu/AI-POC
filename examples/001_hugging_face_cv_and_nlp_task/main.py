f74e2c35a5c6957b8b68f0af8a516e94f8448970
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2024-07-01 14:59:07 +0200

    Add new file img. Add labeling of image data. Add show img func.

--- examples/001_hugging_face_cv_and_nlp_task/main.py
+++ examples/001_hugging_face_cv_and_nlp_task/main.py
@@ -31,8 +31,8 @@ API_URL = "https://api-inference.huggingface.co/models/facebook/detr-resnet-50"
     stop=stop_after_attempt(5),
     retry=retry_if_exception_type(requests.exceptions.RequestException)
 )
-def get_cv_data(filename: str):
-    with open(filename, "rb") as f:
+def get_cv_data(image_path: str):
+    with open(image_path, "rb") as f:
         data = f.read()
     response = requests.request("POST", API_URL, headers=HEADERS, data=data)
     data = json.loads(response.content.decode("utf-8"))
@@ -44,9 +44,15 @@ def get_cv_data(filename: str):
 
     return data
 
-def get_image_data(filename: str):
-    image = cv2.imread(filename)
 
+
+def get_image(image_path: str, to_show: bool = True):
+    image = cv2.imread(image_path)
+    return image
+
+def show_image(image = None, image_path = None):
+    if image_path:
+        image = get_image(image_path)
     #Show image
     if is_interactive:
         pass
@@ -54,8 +60,10 @@ def get_image_data(filename: str):
         # cv2_imshow(image)
     else:
         # cv2.imshow('Savanna Image', image)
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB)) # Convert the image from BGR to RGB color space
-        plt.title("Savanna Image")
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), interpolation='none') # Convert the image from BGR to RGB
+        # color
+        # space
+        plt.title("Image repr")
         plt.show()
 
         # Wait for a key press and close the window
@@ -63,9 +71,36 @@ def get_image_data(filename: str):
         cv2.destroyAllWindows()
 
 
+def add_cv_data_to_image(image, cv_data):
+    for result in cv_data:
+        box = result["box"]
+        xmin = box["xmin"]
+        ymin = box["ymin"]
+        xmax = box["xmax"]
+        ymax = box["ymax"]
+        label = result["label"]
+
+        red_color = (255, 50, 50)
+
+        # Draw a line between the top-left and bottom-right corners of the bounding box.
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), red_color, 2)
+
+        # Draw the label.
+        cv2.putText(
+            image, label,
+            (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX,
+            1, red_color, 2)
+
+    return image
+
 
 if __name__ == "__main__":
-    image = "savanna.jpg"
-    image_data = get_image_data(image)
-    cv_data = get_cv_data(image)
-    print(image_data)
+    image_path = "savanna2.jpg"
+    image = get_image(image_path)
+    # Show original image
+    # show_image(image)
+    # Get CV data
+    image_cv_data = get_cv_data(image_path)
+    image_labeled = add_cv_data_to_image(image, image_cv_data)
+    # Show labeled image
+    show_image(image_labeled)
