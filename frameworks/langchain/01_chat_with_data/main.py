fda8a9a224b88105b9046000259606cb4d65b8a7
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2024-07-07 01:17:04 +0200

    Add notion and web data

--- frameworks/langchain/01_chat_with_data/main.py
+++ frameworks/langchain/01_chat_with_data/main.py
@@ -3,13 +3,16 @@ import openai
 import sys
 from dotenv import load_dotenv, find_dotenv
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
-from langchain.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import YoutubeAudioLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import NotionDirectoryLoader
+
+from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.parsers.audio import OpenAIWhisperParser
 
 from pathlib import Path
-from langchain.document_loaders.generic import GenericLoader
-from langchain.document_loaders.parsers import OpenAIWhisperParser
+
 
 
 sys.path.append('../..')
@@ -21,12 +24,33 @@ openai.api_key = OPENAI_API_KEY
 current_dir = Path.cwd()
 current_dir_parent = current_dir.parent
 
+
+
+# Notion
+notion_path = current_dir_parent / "data/docs/md"
+
+notion_loader = NotionDirectoryLoader(str(notion_path))
+notion_data = notion_loader.load()
+notion_meta = notion_data[0].metadata
+print(notion_meta)
+
 # PDF
 
 pdf_path = current_dir_parent / "data/docs/pdf/MachineLearning-Lecture01.pdf"
 pdf_loader = PyPDFLoader(str(pdf_path))
 pdf_data = pdf_loader.load()
 assert len(pdf_data) == 22
+page = pdf_data[3]
+print(page.page_content[:100])
+print(page.metadata)
+
+# URLs (web data)
+md_url = 'https://github.com/langchain-ai/langchain/blob/master/README.md'
+web_loader = WebBaseLoader(md_url)
+web_data = web_loader.load()
+web_data_snippet = web_data[0].page_content[500:600]
+print(web_data_snippet)
+
 
 # YouTube
 yt_path = current_dir_parent / "data/youtube/url1.url"
@@ -39,9 +63,4 @@ yt_loader = GenericLoader(
     blob_parser=OpenAIWhisperParser()
 )
 yt_data = yt_loader.load()
-
-# URLs (web data)
-md_url = 'https://github.com/langchain-ai/langchain/blob/master/README.md'
-web_loader = WebBaseLoader(md_url)
-web_data = web_loader.load()
-web_data_snippet = web_data[0].page_content[500:]
+print(yt_data)
