66f07ede7ca82a361f88c10243c36b68180a1cc2
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2024-07-07 00:47:07 +0200

    Add youtube load for frameworks/langchain/01_chat_with_data

---
+++ frameworks/langchain/01_chat_with_data/main.py
@@ -0,0 +1,41 @@
+import os
+import openai
+import sys
+from dotenv import load_dotenv, find_dotenv
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+
+from pathlib import Path
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+
+
+sys.path.append('../..')
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+# OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+OPENAI_API_KEY = os.environ.get('API_KEY_OPEN_AI')
+openai.api_key = OPENAI_API_KEY
+current_dir = Path.cwd()
+current_dir_parent = current_dir.parent
+
+# PDF
+
+pdf_path = current_dir_parent / "data/docs/pdf/MachineLearning-Lecture01.pdf"
+pdf_loader = PyPDFLoader(str(pdf_path))
+pdf_data = pdf_loader.load()
+assert len(pdf_data) == 22
+
+# YouTube
+yt_path = current_dir_parent / "data/youtube/url1.url"
+with open(yt_path, 'r') as f:
+    yt_path_str = f.read()
+
+tmp_dir = current_dir_parent / "tmp"
+yt_loader = GenericLoader(
+    blob_loader=YoutubeAudioLoader([yt_path_str], str(tmp_dir)),
+    blob_parser=OpenAIWhisperParser()
+)
+yt_data = yt_loader.load()
+
+g = 1
