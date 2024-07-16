0eb7b0d9b751d7b1db64228ce091825973d05a3f
Alexander Nesterovich <a.v.nesterovich@gmail.com>
2024-07-16 15:52:43 +0200

    Add common.cfg.get_data decorator ot load of dump pickle data automatically for funcs

--- common/cfg.py
+++ common/cfg.py
@@ -1,9 +1,12 @@
 import os
 import pickle
+from functools import wraps
 from pathlib import Path
 
 import torch
 from dotenv import find_dotenv, load_dotenv
+import inspect
+import openai
 
 
 # Load environment variables
@@ -14,6 +17,8 @@ COHERE_API_KEY = os.environ.get("COHERE_API_KEY")
 HUGGINGFACEHUB_API_TOKEN = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 SERPAPI_API_KEY = os.environ.get("SERPAPI_API_KEY")
 LANGCHAIN_API_KEY = os.environ.get("LANGCHAIN_API_KEY")
+
+openai.api_key = OPENAI_API_KEY
 
 
 # Set paths and cfg parts
@@ -59,7 +64,35 @@ def load_data(data_path: Path):
     return res
 
 
-def get_func_meta(func, current_dir):
-    f_name = get_func_meta.__name__
+def get_func_data_path(func, current_dir):
+    f_name = get_func_data_path.__name__
     data_path = current_dir / f'{f_name}.pkl'
-    return current_dir
+    return data_path
+
+
+
+def get_data(file = None, load = True, *args, **kwargs):
+    caller_file = file if file else inspect.stack()[1].filename
+
+    def decorator(func):
+
+        def get_data_path(func):
+            current_dir = Path(caller_file).parent.resolve()
+            func_name = func.__name__
+            data_path = current_dir / f'{func_name}.pkl'
+            return data_path
+
+        data_path = get_data_path(func)
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if data_path.exists() and load:
+                res = load_data(data_path)
+            else:
+                res = func(*args, *kwargs)
+                dump_data(res, data_path)
+            return res
+
+        return wrapper
+
+    return decorator
--- frameworks/langchain/overview/diff_llm_providers/diff_llms.py
+++ frameworks/langchain/overview/diff_llm_providers/diff_llms.py
@@ -1,14 +1,22 @@
 from types import SimpleNamespace
 
-from langchain.llms import Cohere
+
+from langchain.indexes import VectorstoreIndexCreator
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms.ctransformers import CTransformers
+from langchain_community.vectorstores import DocArrayInMemorySearch
 from langchain_core.prompts import PromptTemplate
-# from langchain_cohere.llms import Cohere
-from langchain_openai import OpenAI, ChatOpenAI
+# from langchain.llms import Cohere
+from langchain_cohere.llms import Cohere
+# from langchain_cohere import ChatCohere
+from langchain_openai import OpenAI, ChatOpenAI, OpenAIEmbeddings
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.schema import (
     HumanMessage
 )
+from langchain_community.document_loaders import TextLoader
+from sentence_transformers import SentenceTransformer
+
 
 from common.cfg import *
 
@@ -17,9 +25,11 @@ current_dir_parent = current_dir.parent
 
 # https://python.langchain.com/v0.2/docs/integrations/providers/
 
-chatgpt = ChatOpenAI(model_name='gpt-3.5-turbo')
+chatgpt = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0.1)
 # https://platform.openai.com/docs/deprecations
 gpt3 = OpenAI(model_name='gpt-3.5-turbo-instruct')
+embeddings = OpenAIEmbeddings()
+# embedding2 = SentenceTransformer('all-MiniLM-L6-v2')
 # https://python.langchain.com/v0.2/docs/integrations/providers/cohere/
 cohere = Cohere(model='command-xlarge')
 # https://python.langchain.com/v0.2/docs/integrations/providers/serpapi/
@@ -37,10 +47,14 @@ bloom = HuggingFacePipeline.from_model_id(
 human_msg_txt = 'What is a AI applications developer roadmap in 2024?'
 human_msg = HumanMessage(content=human_msg_txt)
 
-def ask_qns_via_model(load=False):
-    d_path = current_dir / f'{ask_qns_via_model.__name__}.pkl'
-    if load:
-        return load_data(d_path)
+human_msg2_txt = 'Make short summary for this document'
+
+@get_data(load=True)
+def ask_questions_using_diff_provider_models():
+    # get_func_data_path(ask_questions_using_diff_provider_models)
+    # d_path = current_dir / f'{ask_questions_using_diff_provider_models.__name__}.pkl'
+    # if load:
+    #     return load_data(d_path)
     qn_answers = SimpleNamespace()
     try:
         qn_answers.chatgpt_res = chatgpt([human_msg])
@@ -62,15 +76,34 @@ def ask_qns_via_model(load=False):
         qn_answers.mistral_res = mistral(human_msg_txt)
     except Exception as ex:
         print(ex)
+        return qn_answers
         # raise ex
-    finally:
-        res = dump_data(qn_answers, d_path)
-        return res
+    # finally:
+    #     qn_answers = dump_data(qn_answers, d_path)
+    #     return qn_answers
+
+
+@get_data(load=True)
+def qa_using_external_doc():
+    """
+    LineChain is a tool that aids in breaking down your document into smaller sections. You will create embedding vectors for each of these text segments. When a question is asked, LineChain will perform a semantic search to determine which text segment vectors closely match the question vector. It then retrieves the relevant text segment based on the question. This process ensures that the most applicable text is referred to when answering the question based on the document or text.
+
+    There are a lot of document loaders: File Loader, Directory Loader, Notion, ReadTheDocs, HTML, PDF, PowerPoint, Email, GoogleDrive, Obsidian, Roam, EverNote, YouTube, Hacker News, GitBook, S3 File, S3 Directory, GCS File, GCS Directory, Web Base, IMSDb, AZLyrics, College Confidential, Gutenberg, Airbyte Json, CoNLL-U, iFixit, Notebook, Copypaste, CSV, Facebook Chat, Image, Markdown, SRT, Telegram, URL, Word Document, Blackboard
+    """
+    # https://langchain-cn.readthedocs.io/en/latest/modules/indexes/document_loaders.html
+    txt_loader = TextLoader(txt1_path)
+    txt_index = VectorstoreIndexCreator(
+        embedding=embeddings
+    ).from_loaders([txt_loader])
+
+    res = txt_index.query(human_msg2_txt, llm=gpt3)
+    return res
 
 
 def execute():
-    res1 = ask_qns_via_model(load=True)
-    g = 1
+    res1 = ask_questions_using_diff_provider_models()
+    res2 = qa_using_external_doc()
+    g = 2
 
 
 if __name__ == '__main__':
